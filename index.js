2954d79c15eac864add2ddc0aae1470c98ee500f
Tolu Akinrinola <remmakins@gmail.com>
2023-04-10 05:27:41 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, letters) {
+  let lengthOfLetters = letters.length;
+  return drivers.filter(function (driverName) {
+    return driverName.slice(0, lengthOfLetters) === letters;
+  });
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
